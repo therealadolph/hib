52c63935a764954ff41cf2a0ae410070becf5606
therealadolph <91224915+therealadolph@users.noreply.github.com>
2022-11-15 12:11:13 +0000

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,41 @@
+#my invitee list
+guest_list = ["Akua Mansuwa" , "Susana Otoo", "Adwoa Pokua"]
+
+#invitation message to each invitee
+print("Hello",guest_list[0],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[1],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[2],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+
+#message indicating some guests will not make it to the diner
+print("Oops", guest_list[1], "and" ,guest_list[2], "cannot it to the party! ")
+
+#replacing guests that will not make it to the diner
+guest_list[1]=("Natasha Darko")
+guest_list[2]=("Festus Osei Kwame")
+
+#invittion message for the new list of invitees, each
+print("Hello",guest_list[0],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[1],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[2],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+
+# list of more invitees
+guest_list.insert(0,"Delpine Ranita")
+guest_list.insert(2,"Callum Scott")
+guest_list.append("Alex Sampson")
+
+print(guest_list)
+
+#message indicating we have a bigger table
+print("Hello people! We just found a bigger table. Join us let us have some diner.")
+
+#new set of invitation messages
+print("Hello",guest_list[0],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[1],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[2],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[3],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[4],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+print("Hello",guest_list[5],"I invite you to my diner party on the 24 December 2022 at Kimpeski Hotel,Accra.")
+
+
+#Adolph's code!
+#Questions 1 and 2
---
+++ assignment2.py
@@ -0,0 +1,27 @@
+name = "Adolph"
+
+print("Hello", name, "you should know that Python is cool but Java is cooler")
+
+first_name = "Wagner"
+
+print(first_name.upper())
+print(first_name.lower())
+print(first_name.title())
+
+print("Alvin Toffler once said \"The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn,unlearn and relearn \"")
+
+famous_quote = "Alvin Toffler"
+
+message = "\"The illiterate of the 21st century will not be those who cannot read and write, but those who cannot learn,unlearn and relearn \""
+
+print (famous_quote, ""+ message)
+
+#What are constants?
+#The term constant in python programming refers to names representing values that don't change during a program's execution.
+# Constants are a fundamental concept in programming, and Python developers use them in many cases.
+
+#What are comments?
+#Comments  in python is the inclusion of short descriptions along with the codes to increase readability.
+# Comments in python usually start with '#' for a single line of comment and triple single quotes '''
+#for a double line of comment.
+
